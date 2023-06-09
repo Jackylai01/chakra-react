dd48e85daf44d420788b585da4e825fdd4ed5d79
jacky.inoby <sn185672@gmail.com>
2023-06-09 16:19:21 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout';
+import Create from './pages/Create';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="create" element={<Create />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,40 @@
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
+  const boxStyles = {
+    p: '10px',
+    bg: 'purple.400',
+    color: 'white',
+    my: '10px',
+    textAlign: 'center',
+  };
+
+  return (
+    <Container as="section" maxWidth="100%">
+      <Heading m="30px" p="10px">
+        Chakra UI Componets
+      </Heading>
+      <Text color="blue.300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique esse
+        molestias ducimus itaque enim laudantium sunt impedit distinctio illo.
+        Eaque non voluptatum nemo perferendis quasi? Impedit animi velit rerum
+        magni!
+      </Text>
+      <Box my="30px" p="30px" bg="orange" as="section">
+        <Text color="blackAlpha.500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias commodi
+          ipsum eius molestias aut eos praesentium hic consequatur et aperiam
+          voluptatem dicta eaque, at neque adipisci, velit, quam impedit
+          doloribus!
+        </Text>
+      </Box>
+      <Box sx={boxStyles} as={Link} to="/profile">
+        JACKY LAI
+      </Box>
+    </Container>
+  );
+};
+
+export default Dashboard;
